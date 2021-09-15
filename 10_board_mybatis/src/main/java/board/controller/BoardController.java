f420725b9712f01b5087971236a2e4220076ef7a
garnet <player225@naver.com>
2021-09-15 23:00:46 +0900

    게시판 CRUD완성

--- 10_board_mybatis/src/main/java/board/controller/BoardController.java
+++ 10_board_mybatis/src/main/java/board/controller/BoardController.java
@@ -5,14 +5,18 @@ import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import board.domain.BoardVO;
 import board.service.BoardService;
 
 @Controller
+@SessionAttributes("boardVO")
 public class BoardController {
 	private BoardService boardService;
 	
@@ -53,13 +57,13 @@ public class BoardController {
 		BoardVO boardVO = boardService.read(seq);
 		model.addAttribute("boardVO", boardVO);
 		return "/board/edit";
-	}
-	
+	}	
 	@RequestMapping(value="/board/edit/{seq}", method=RequestMethod.POST)
 	public String edit(
-			@Valid BoardVO boardVO,
+			@Valid @ModelAttribute BoardVO boardVO,
 			BindingResult result,
 			int pwd,
+			SessionStatus sessionStatus,
 			Model model) {
 		if(result.hasErrors()) {
 			return "/board/edit";
@@ -67,10 +71,36 @@ public class BoardController {
 		else {
 			if(boardVO.getPassword() == pwd) {
 				boardService.edit(boardVO);
+				sessionStatus.setComplete();
 				return "redirect:/board/list";
 			}
 		}
 		model.addAttribute("msg", "비밀번호가 일치하지 않습니다.");
 		return "/board/edit";
+	}
+	
+	//글 삭제 요청을 처리할 메서드
+	@RequestMapping(value="/board/delete/{seq}", method=RequestMethod.GET)
+	public String delete(@PathVariable int seq, Model model) {
+		model.addAttribute("seq", seq);
+		return "/board/delete";
+	}
+	@RequestMapping(value="/board/delete", method=RequestMethod.POST)
+	public String delete(int seq, int pwd, Model model) {
+		int rowCount;
+		BoardVO boardVO = new BoardVO();
+		boardVO.setSeq(seq);
+		boardVO.setPassword(pwd);
+		
+		rowCount = boardService.delete(boardVO);
+		
+		if(rowCount == 0) {
+			model.addAttribute("seq", seq);
+			model.addAttribute("msg", "비밀번호가 일치하지 않습니다.");
+			return "/board/delete";
+		}
+		else {
+			return "redirect:/board/list";
+		}
 	}
 }
